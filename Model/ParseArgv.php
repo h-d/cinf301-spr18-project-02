4461f21b27990554df99dbfed3890f2ec17c52e2
hdevoe <hdevoe@stetson.edu>
2018-02-21 13:25:08 -0500

    Adding comments to code

--- Model/ParseArgv.php
+++ Model/ParseArgv.php
@@ -17,7 +17,7 @@ class ParseArgv
     //parses arg into manageable array
     private function parse($args)
     {
-        //string for checking -(char)
+        //strings for checking -(char) and --(string)=(string)
         $single_check = "/^-[0-9a-zA-Z]$/";
         $double_check = "/^--(.*?)\=(.*?)/";
 
@@ -33,9 +33,11 @@ class ParseArgv
                 //is the arg followed by an arg beginning with "-"?
                 } else if (preg_match("/^-/", $args[$i + 1])) {
                     $this->flagsParsed[] = $args[$i];
+                //if not, create add a single to the singlesParsed array using the breakup_single function
                 } else {
                     $this->breakup_single($args, $i, $args[$i + 1]);
                 }
+            //check if arg matches double_check
             } else if (preg_match($double_check, $args[$i]))
             {
                 $this->breakup_double($args[$i]);
@@ -44,29 +46,37 @@ class ParseArgv
 
     }
 
+
+    //function to break up string following single into vals
     private function breakup_single($args, $i, $string)
     {
+        //create list from comma-delimited string
         $vals = explode(',', $string);
 
+        //add vals from list into singlesParsed array at the given index
         for($n = 0; $n < count($vals);$n++)
         {
             $this->singlesParsed[$args[$i]][] = $vals[$n];
         }
     }
 
+    //function to break up double string
     private function breakup_double($string)
     {
+        //break into name and values
         list($name, $value) = explode('=', $string);
 
+        //break values into a list of vals
         $vals = explode(',', $value);
 
+        //add all vals to the doubleParsed array in the name index
         for($n = 0; $n < count($vals);$n++)
         {
             $this->doublesParsed[$name][] = $vals[$n];
         }
     }
 
-    //return flag array
+    //return all arrays in one mega-array
     public function getParsed()
     {
 
--- testArgs.php
+++ testArgs.php
@@ -1,6 +1,8 @@
 <?php
-require_once "../Model/ParseArgv.php";
+require_once "./Model/ParseArgv.php";
 
+
+//Parse args
 $parsed = new ParseArgv($_SERVER['argv']);
 $parsedArgs = $parsed->getParsed();
 
@@ -9,14 +11,19 @@ foreach ($parsedArgs as $category=>$type)
 {
     print("\n$category\n");
 
+    //if category is flags, only print the flag values
     if($category == 'FLAGS')
     {
         foreach ($type as $name => $value)
         {
+            //replace dashes with '' and print
             $tempval = preg_replace('/\-/',"",$value);
             print("'$tempval'\n");
         }
+
+    //if the category is not flags, print the name and the args belonging to the name
     } else {
+
 
         foreach ($type as $name => $arg) {
 
@@ -27,14 +34,18 @@ foreach ($parsedArgs as $category=>$type)
 
             $fullstring = " ";
 
+
+            //create temp string with all values
             foreach ($arg as $item => $value) {
                 $fullstring = $fullstring . "[$item] '$value', ";
                 $integer = $item + 1;
             }
 
+            //remove final comma and print
             $partstring = substr($fullstring, 0, -2);
             print($partstring);
 
+            //print # of args
             if ($integer > 1) {
                 print(" ($integer arguments)\n");
             } else print(" ($integer argument)\n");
